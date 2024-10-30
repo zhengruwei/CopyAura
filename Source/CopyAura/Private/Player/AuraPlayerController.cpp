9b62f3ba789d31fe6244de579813d1c396009a74
zhengruwei <zhengruwei@139.com>
2024-10-31 00:36:21 +0800

    添加输入映射上下文

---
+++ Source/CopyAura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,29 @@
+// Copyright CopyAura zrw
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if(Subsystem)
+	{
+		Subsystem->AddMappingContext(AuraContext,0);
+
+		bShowMouseCursor = true;
+		DefaultMouseCursor = EMouseCursor::Default();
+
+		FInputModeGameAndUI InputModeData;
+		InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+		InputModeData.SetHideCursorDuringCapture(false);
+		SetInputMode(InputModeData);
+	}
+}
---
+++ Source/CopyAura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,26 @@
+// Copyright CopyAura zrw
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
+
+/**
+ * 
+ */
+UCLASS()
+class COPYAURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere,Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+};
