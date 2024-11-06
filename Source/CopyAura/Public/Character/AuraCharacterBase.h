f9c4683b06e171fabe1a13fe2b1c4c13cfe05e62
zhengruwei <zhengruwei@139.com>
2024-11-06 22:34:29 +0800

    给玩家状态类（AuraPlayerState)和敌人类（AuraEnemy）设定复制模式（SetReplicationMode）

--- Source/CopyAura/Public/Character/AuraCharacterBase.h
+++ Source/CopyAura/Public/Character/AuraCharacterBase.h
@@ -4,21 +4,31 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
 #include "AuraCharacterBase.generated.h"
 
+class UAttributeSet;
+class UAbilitySystemComponent;
+
 UCLASS(Abstract)
-class COPYAURA_API AAuraCharacterBase : public ACharacter
+class COPYAURA_API AAuraCharacterBase : public ACharacter,public IAbilitySystemInterface
 {
     GENERATED_BODY()
 
 public:
     // Sets default values for this character's properties
     AAuraCharacterBase();
-
+    virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+    UAttributeSet* GetAttributeSet() const {return AttributeSet;}
 protected:
     // Called when the game starts or when spawned
     virtual void BeginPlay() override;
     UPROPERTY(EditAnywhere,Category = "Combat")
     TObjectPtr<USkeletalMeshComponent> Weapon;
+
+    UPROPERTY()
+    TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+    UPROPERTY()
+    TObjectPtr<UAttributeSet> AttributeSet;
 
 };
