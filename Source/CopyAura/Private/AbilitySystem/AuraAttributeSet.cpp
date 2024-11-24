6af2deb63dd6862758ee8d3b07834d8d06474201
zhengruwei <zhengruwei@139.com>
2024-11-24 23:17:50 +0800

    优化进度条效果，修正限制血量值和魔法值的功能，游戏效果执行需重新设定这两个值

--- Source/CopyAura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/CopyAura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -79,7 +79,15 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectMo
 
   FEffectProperties Properties;
   SetEffectProperties(Data,Properties);
-  
+
+  if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+  {
+    SetHealth(FMath::Clamp(GetHealth(),0.f,GetMaxHealth()));
+  }
+  if (Data.EvaluatedData.Attribute == GetManaAttribute())
+  {
+    SetMana(FMath::Clamp(GetMana(),0.f,GetMaxMana()));
+  }
 }
 
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
