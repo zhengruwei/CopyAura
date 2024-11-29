04a1d213b78662502343ac32e569858181ae0f35
zhengruwei <zhengruwei@139.com>
2024-11-29 20:08:09 +0800

    添加自定义计算方法类，实现最大魔法值基于智力和等级而变化；实现利用游戏效果初始化健康值和魔法值。

--- Source/CopyAura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/CopyAura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -10,8 +10,6 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-  InitHealth(10.f);
-  InitMana(10.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
